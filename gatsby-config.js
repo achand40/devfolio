41cb31b4ff5753baad1d0232224ec58319d1fe59
achand40 <116143192+achand40@users.noreply.github.com>
2022-10-19 01:52:34 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,19 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Abhijith Chandran',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Abhijith Chandran's Personal Website`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Applied-Math + CS @Brown.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    //  author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/achand40`,
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I write c`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
